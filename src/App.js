38fc38fe380b9dee2f9f2921d24065c44f55bec7
Ezekiel Saturday <ezekielsaturday@Doro-Macbook.local>
2019-12-09 19:49:05 +0100

    initial set up for react todo app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  state = {
+      todos : [
+        {id : 1, content: "Buy some milk"},
+        {id: 2, content: "Play mario Kart"}
+      ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+  
+      </div>
+    );
+  }
+}
+
+export default App;
